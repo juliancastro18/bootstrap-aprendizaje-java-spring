82b2d3ba6bd8ce3f51d39c41a64cf0fb70ffd3db
Julian Castro <jcast18k@gmail.com>
2021-04-24 18:00:01 -0300

    adding spring files

---
+++ src/test/java/com/unla/juliancastro18/Juliancastro18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.juliancastro18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Juliancastro18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
